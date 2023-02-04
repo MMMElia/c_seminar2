02a26a9787be376dca346b90abbe7376b91314e7
Elya Megrabyan <m-elia@yandex.ru>
2023-02-04 23:48:06 +0300

    14

---
+++ 14/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a%7==0 && a%23==0) {Console.Write($"Число {a} кратно 7 и 23.");}
+else {Console.Write($"Число {a} не кратно 7 и 23 одновременно.");}
+
