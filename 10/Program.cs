3e4f8467819d8a0d614d165c9237301d9901822c
Elya Megrabyan <m-elia@yandex.ru>
2023-02-04 23:15:59 +0300

    10

---
+++ 10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+int n = new Random().Next(100,1000);
+
+Console.Write(n);
+
+int n1=n/100;
+// int n2=n/10;
+int n3=n % 10;
+int x=n1*10+n3;
+
+Console.Write(" ");
+Console.Write($"Ответ {x}");
