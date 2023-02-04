dbb1e737ec6a7e0971fc10bbd58dc9c83f4f1abe
Elya Megrabyan <m-elia@yandex.ru>
2023-02-04 23:39:10 +0300

    12

---
+++ 12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a%b==0) {Console.Write($"{a} кратно {b}");}
+else Console.Write($"{a} не кратно {b}, остаток от деления равен {a%b}");
+
+
