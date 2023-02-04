e114511f82b39119f6e65ba5af10034f90e4e9e0
Elya Megrabyan <m-elia@yandex.ru>
2023-02-04 23:05:34 +0300

    09

--- 09/Program.cs
+++ 09/Program.cs
@@ -1,1 +1,16 @@
-﻿//
+﻿Console.Clear();
+
+int n = new Random().Next(10,100);
+
+Console.Write(n);
+
+int n1=n/10;
+int n2=n%10;
+int max=n2;
+if (n1>n2) {max = n1;} 
+
+Console.Write(" ");
+Console.Write(max);
+
+
+
