8a5a0b9deca4154d3e5363f96c9c46d1231d1f59
Elya Megrabyan <m-elia@yandex.ru>
2023-02-04 23:58:07 +0300

    16

---
+++ 16/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a*a==b) {
+    Console.Write($"{b} является квадратом {a}");
+    }
+else {
+    if (b*b==a) {
+        Console.Write($"{a} является квадратом {b}");
+        }
+    else {
+        Console.Write("Одно число не является квадратом другого");
+        }
+
+}
