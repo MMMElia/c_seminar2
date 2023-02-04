992394aa1c6a58ba24a83266d388076727145abf
Elya Megrabyan <m-elia@yandex.ru>
2023-02-05 01:05:24 +0300

    Домашнее задание 13

---
+++ DZ_13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+// что третьей цифры нет.
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a<100) {
+    Console.Write("Третьей цифры нет");
+     }
+     else {string b=Convert.ToString(a);
+
+    Console.Write(b[2]);}
+    
